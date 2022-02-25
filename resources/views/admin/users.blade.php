e7d9167be1d6b85b0bd239df2370ca812bf5e6ed
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-02-25 23:27:27 +0500

    Admin

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+<x-app-layout>     
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        @include('admin.admincss')
+    </head>
+    <body>
+        <div class="container-scroller">
+            @include('admin.navbar')
+<h1>USERRR</h1>
+        </div>
+            
+        @include('admin.adminscript')
+    </body>
+</html>
+</body>
+</html>
