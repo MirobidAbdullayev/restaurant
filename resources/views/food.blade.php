3a71e68c52b03a978da1677a9005959df64a8d5e
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-04-12 23:26:19 +0500

    add cart

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -14,18 +14,23 @@
             <div class="col-lg-12">
                 <div class="owl-menu-item owl-carousel">
                     @foreach ($data as $data)
-                    <div class="item">
-                        <div style="background-image: url('/foodimage/{{$data->image}}');" class='card'>
-                            <div class="price"><h6>{{$data->price}}</h6></div>
-                            <div class='info'>
-                              <h1 class='title'>{{$data->title}}</h1>
-                              <p class='description'>{{$data->description}}</p>
-                              <div class="main-text-button">
-                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                              </div>
+                    <form action="{{url('/addcart')}}" method="post">
+                        @csrf
+                        <div class="item">
+                            <div style="background-image: url('/foodimage/{{$data->image}}');" class='card'>
+                                <div class="price"><h6>{{$data->price}}</h6></div>
+                                <div class='info'>
+                                <h1 class='title'>{{$data->title}}</h1>
+                                <p class='description'>{{$data->description}}</p>
+                                <div class="main-text-button">
+                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                                </div>
+                                </div>
                             </div>
+                            <input type="number" name="quality" min="0" style="width: 80px;">
+                            <input type="submit" value="add cart">
                         </div>
-                    </div>
+                    </form>
                     @endforeach
                 </div>
             </div>
